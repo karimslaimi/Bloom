3b5c2c990f5e57d6fec1f7d55a1a3cb0225ffc56
karim <karim-nar@live.fr>
2020-05-30 13:07:21 +0100

    finished everything just i have to install the rating bundle

---
+++ src/DoraBundle/Controller/RatingController.php
@@ -0,0 +1,163 @@
+<?php
+
+namespace DoraBundle\Controller;
+
+use DoraBundle\Entity\Rating;
+use DoraBundle\Form\RatingType;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Rating controller.
+ *
+ * @Route("rating")
+ */
+class RatingController extends Controller
+{
+    /**
+     * Lists all rating entities.
+     *
+     * @Route("/", name="rating_index")
+     * @Method("GET")
+     */
+    public function indexAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $ratings = $em->getRepository('DoraBundle:Rating')->findAll();
+        $average=$em->getRepository(Rating::class)->getratings();
+        $rating = new Rating();
+        $form = $this->createForm('DoraBundle\Form\RatingType', $rating)->add('rating',\blackknight467\StarRatingBundle\Form\RatingType::class, [
+            //...
+            'label' => "Rating",
+            //...
+        ]);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($rating);
+            $em->flush();
+
+            return $this->redirectToRoute('rating_show', array('id' => $rating->getId()));
+        }
+
+
+        return $this->render('@Dora/rating/index.html.twig', array(
+            'ratings' => $ratings,
+            "count"=>count($ratings),
+            "average"=>$average, 'rating' => $rating,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Creates a new rating entity.
+     *
+     * @Route("/new", name="rating_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request)
+    {
+        $rating = new Rating();
+        $form = $this->createForm('DoraBundle\Form\RatingType', $rating)->add('rating',\blackknight467\StarRatingBundle\Form\RatingType::class, [
+            //...
+            'label' => "Rating",
+            //...
+        ]);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($rating);
+            $em->flush();
+
+            return $this->redirectToRoute('rating_show', array('id' => $rating->getId()));
+        }
+
+        return $this->render('@Dora/rating/new.html.twig', array(
+            'rating' => $rating,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a rating entity.
+     *
+     * @Route("/{id}", name="rating_show")
+     * @Method("GET")
+     */
+    public function showAction(Rating $rating)
+    {
+        $deleteForm = $this->createDeleteForm($rating);
+
+        return $this->render('@Dora/rating/show.html.twig', array(
+            'rating' => $rating,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing rating entity.
+     *
+     * @Route("/{id}/edit", name="rating_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, Rating $rating)
+    {
+        $deleteForm = $this->createDeleteForm($rating);
+        $editForm = $this->createForm('DoraBundle\Form\RatingType', $rating);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('rating_edit', array('id' => $rating->getId()));
+        }
+
+        return $this->render('@Dora/rating/edit.html.twig', array(
+            'rating' => $rating,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a rating entity.
+     *
+     * @Route("/{id}", name="rating_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, Rating $rating)
+    {
+        $form = $this->createDeleteForm($rating);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($rating);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('rating_index');
+    }
+
+    /**
+     * Creates a form to delete a rating entity.
+     *
+     * @param Rating $rating The rating entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Rating $rating)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('rating_delete', array('id' => $rating->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+}
---
+++ src/DoraBundle/Entity/Rating.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace DoraBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Rating
+ *
+ * @ORM\Table(name="rating")
+ * @ORM\Entity(repositoryClass="DoraBundle\Repository\RatingRepository")
+ */
+class Rating
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+
+    /**
+     * @ORM\Column(name="comment",type="string")
+     */
+    private $comment;
+
+    /**
+     * @ORM\Column(name="rate",type="integer")
+     */
+    private $rating;
+
+    /**
+     * @ORM\Column(name="date",type="datetime")
+     */
+    private $date;
+
+    /**
+     * @ORM\Column(name="client",type="string")
+     */
+    private $client;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getComment()
+    {
+        return $this->comment;
+    }
+
+    /**
+     * @param mixed $comment
+     */
+    public function setComment($comment)
+    {
+        $this->comment = $comment;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRating()
+    {
+        return $this->rating;
+    }
+
+    /**
+     * @param mixed $rate
+     */
+    public function setRating($rate)
+    {
+        $this->rating = $rate;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * @param mixed $date
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getClient()
+    {
+        return $this->client;
+    }
+
+    /**
+     * @param mixed $client
+     */
+    public function setClient($client)
+    {
+        $this->client = $client;
+    }
+
+
+
+
+
+}
+
